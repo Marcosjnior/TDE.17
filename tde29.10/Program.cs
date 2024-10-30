8e5cfa321e42bf42688a229ab173dc01f9fa593a
Marcosjnior <165732065+Marcosjnior@users.noreply.github.com>
2024-10-29 21:25:48 -0300

    Add files via upload

---
+++ tde29.10/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine(ConversorTemperatura.CelsiusParaFahrenheit(39));
+Console.WriteLine(ConversorTemperatura.FahrenheitParaCelsius(102.5));
+
+Configuracoes.NomeAplicacao = "Laboratorio de Programação";
+Configuracoes.Versao = 5;
+
+Console.WriteLine(Configuracoes.NomeAplicacao);
+Console.WriteLine(Configuracoes.Versao);
